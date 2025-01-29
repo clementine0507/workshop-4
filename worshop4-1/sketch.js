dbd5fcc1b4b890e3c8a3658211808d356313d01a
Clementine <wtan387@aucklanduni.ac.nz>
2025-01-30 01:53:24 +0800

    Update sketch.js

--- worshop4-1/sketch.js
+++ worshop4-1/sketch.js
@@ -5,23 +5,20 @@ let listInputs = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // 输入框
+
   input = createInput();
   input.position(100, 100);
   submitButton = createButton('submit');
-  // 将提交按钮放在输入框旁边
   submitButton.position(input.x + input.width, input.y);
   submitButton.mousePressed(submitInput);
-  // 创建一个二级标题
   greeting = createElement('h2', 'Please write your question.');
   greeting.style('color', 'black');
-  // 将标题放在输入框上方
   greeting.position(input.x, input.y - 50);
 }
 
 function submitInput() {
   let userInput = input.value();
-  input.value(''); // 将输入值重置为空字符串
+  input.value(''); 
   listInputs.push(userInput);
   if (listInputs.length > 10) {
     listInputs.shift();
